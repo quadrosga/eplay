4082e6762ff4f4746f2992b006554603b6e5887e
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2025-01-01 16:08:13 -0300

    criando o carrinho

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,51 @@
+import Button from '../Button'
+import Tag from '../Tag'
+import {
+  CardContainer,
+  Overlay,
+  Sidebar,
+  Prices,
+  Quantity,
+  CartItem
+} from './styles'
+
+const Cart = () => (
+  <CardContainer>
+    <Overlay />
+    <Sidebar>
+      <ul>
+        <CartItem>
+          <img src="https://placehold.co/80x80" />
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS4</Tag>
+            <span>R$ 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+        <CartItem>
+          <img src="https://placehold.co/80x80" />
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS4</Tag>
+            <span>R$ 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+      </ul>
+      <Quantity>2 jogo(s) no carrinho</Quantity>
+      <Prices>
+        Total de R$ 250,00
+        <br />
+        <span>em até 6x sem juros</span>
+      </Prices>
+      <Button type="button" title="Clique aqui para continuar com a compra">
+        Continuar com a compra
+      </Button>
+    </Sidebar>
+  </CardContainer>
+)
+
+export default Cart
---
+++ src/components/Cart/styles.ts
@@ -0,0 +1,93 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
+import { ButtonContainer } from '../Button/styles'
+import fechar from '../../assets/images/close1.png'
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+`
+export const CardContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  z-index: 1;
+`
+export const Sidebar = styled.aside`
+  background-color: ${cores.cinza};
+  color: ${cores.branca};
+  z-index: 1;
+  padding: 40px 16px 0 16px;
+  font-weight: bold;
+  max-width: 360px;
+  width: 100%;
+
+  ${ButtonContainer} {
+    max-width: 100%;
+    width: 100%;
+  }
+`
+export const Prices = styled.p`
+  margin-bottom: 24px;
+
+  span {
+    display: block;
+    font-size: 12px;
+    color: ${cores.cinzaClaro};
+  }
+`
+export const Quantity = styled.p`
+  font-size: 16px;
+  margin-top: 32px;
+  margin-bottom: 16px;
+`
+export const CartItem = styled.li`
+  display: flex;
+  border-bottom: 1px solid ${cores.cinzaClaro};
+  padding: 8px 0;
+  position: relative;
+
+  img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    margin-right: 24px;
+  }
+
+  h3 {
+    color: ${cores.branca};
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+  span {
+    display: block;
+    color: ${cores.branca};
+    font-weight: bold;
+    font-size: 14px;
+  }
+
+  ${TagContainer} {
+    margin: 8px 8px 16px 0;
+  }
+
+  button {
+    background-image: url(${fechar});
+    width: 16px;
+    height: 16px;
+    border: none;
+    background-color: transparent;
+    position: absolute;
+    top: 8px;
+    right: 0;
+  }
+`
